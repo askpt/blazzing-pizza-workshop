3cabd4b29aadb39e311f574402b499f0c4239e97
André Silva <2493377+askpt@users.noreply.github.com>
2020-06-01 12:04:57 +0100

    Finished lesson 06.

---
+++ BlazingPizza.Client/PizzaAuthenticationState.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace BlazingPizza.Client
+{
+    public class PizzaAuthenticationState : RemoteAuthenticationState
+    {
+        public Order Order { get; set; }
+        
+    }
+}
