379268c94eebe8f236270794a44ff5fa7b17b89b
André Silva <2493377+askpt@users.noreply.github.com>
2020-06-07 14:21:55 +0100

    Finished lesson 7

---
+++ BlazingPizza.Client/JsRuntimeExtensions.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace BlazingPizza.Client
+{
+    public static class JsRuntimeExtensions
+    {
+        public static ValueTask<bool> Confirm(this IJSRuntime jsRuntime, string message)
+        {
+            return jsRuntime.InvokeAsync<bool>("confirm", message);
+        }
+    }
+}
